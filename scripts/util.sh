5580bcac2329a4a554696cacc0c7479e6a2b942c
Joris Klein Tijssink <noreply@joris.me>
2023-08-20 15:23:24 +0200

    Add resolved debug

--- scripts/util.sh
+++ scripts/util.sh
@@ -3,9 +3,10 @@
 function require () {
     echo "Require: $1"
     var=$1
-    echo "Var: $var"
-    echo "Var resolved: ${!var}"
-    if [ -z ${!var} ]; then
+    echo "Name: $var"
+    resolved=${!var}
+    echo "Resolved: $resolved"
+    if [ -z "${!var}" ]; then
         echo "Missing required environment variable: \$$1"
         exit 1
     fi;
