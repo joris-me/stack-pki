f700e3bc4eeb26c962c5f4f6b851cd17e1b20420
Joris Klein Tijssink <noreply@joris.me>
2023-08-20 16:22:37 +0200

    Fix error message in utils/require

--- scripts/util.sh
+++ scripts/util.sh
@@ -4,7 +4,7 @@ function require () {
     name=$1
     resolved=${!name}
     if [ -z "$resolved" ]; then
-        echo "Missing required environment variable: \$name"
+        echo "Missing required environment variable: \$$name"
         exit 1
     fi;
 }
