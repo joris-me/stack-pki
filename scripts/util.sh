c989bf310a707012a0fb4519fe7b71dc4a7d9bfd
Joris Klein Tijssink <noreply@joris.me>
2023-08-20 15:22:13 +0200

    Add debugging to util.sh

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 function require () {
+    echo "Require: $1"
     var=$1
+    echo "Var: $var"
+    echo "Var resolved: ${!var}"
     if [ -z ${!var} ]; then
         echo "Missing required environment variable: \$$1"
         exit 1
